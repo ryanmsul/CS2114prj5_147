d4b392e5e9525d51beed28104e8e48917cfe7f0b
MathewsDavid <davidm05@vt.edu>
2024-04-05 17:16:38 -0400

    Update ryanmsul12.java
    edit to ryanmsul

--- src/prj5/ryanmsul12.java
+++ src/prj5/ryanmsul12.java
@@ -6,4 +6,9 @@ public class ryanmsul12
     {
         System.out.println("hello world");
     }
+    public static void main2(String[] args) {
+
+        System.out.println("Glad to be on your team, from David");
+
+    }     
 }

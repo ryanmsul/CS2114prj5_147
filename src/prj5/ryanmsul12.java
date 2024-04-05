c13d1f77c44776feb6e323967e42faade400cdf0
ryanmsul <ryanmsul12@vt.edu>
2024-04-05 17:15:45 -0400

    Checking for merge error
    checking to see if we get a merge error

--- src/prj5/ryanmsul12.java
+++ src/prj5/ryanmsul12.java
@@ -6,4 +6,10 @@ public class ryanmsul12
     {
         System.out.println("hello world");
     }
+    public static void main2(String[] args) 
+    {
+
+        System.out.println("Glad to be on your team, from Colby Ryan");
+
+    }
 }

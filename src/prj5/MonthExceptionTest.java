b54e742a0927d224d5346e91ada7b2bef1f0e276
MathewsDavid <davidm05@vt.edu>
2024-04-18 21:08:59 -0400

    Create MonthExceptionTest.java

---
+++ src/prj5/MonthExceptionTest.java
@@ -0,0 +1,49 @@
+package prj5;
+// -------------------------------------------------------------------------
+/**
+ *  Write a one-sentence summary of your class here.
+ *  Follow it with additional details about its purpose, what abstraction
+ *  it represents, and how to use it.
+ * 
+ *  @author ddmat
+ *  @version Apr 18, 2024
+ */
+public class MonthExceptionTest extends student.TestCase
+{
+   
+    //~ Fields ................................................................
+    /**
+     * influencer for testing
+     */
+    Influencer i;
+    //~ Constructors ..........................................................
+
+    //~Public  Methods ........................................................
+
+    public void setUp() {
+        i = new Influencer("UserName","Channelname", "Serbia", "Ibogaine");
+        
+    }
+    
+    // ----------------------------------------------------------
+    /**
+     * Test month exception 
+     */
+    public void testMonthException() {
+        
+        Exception thrown = null;
+        
+        try {
+            i.addMonthActivity(new Month("Movember",1,2,3,4,5));
+        }
+        catch(Exception e) {
+            thrown = e;
+        }
+        
+        //assertNotNull(thrown);
+        
+        assertTrue(thrown instanceof MonthException);
+        
+        
+    }
+}

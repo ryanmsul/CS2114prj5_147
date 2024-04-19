015f104d7752ee2d2770e21e36c4c273104ffb7d
jimenezjessej05 <jimenezjessej05@vt.edu>
2024-04-18 20:35:07 -0400

    influencer test header restored
    bagong bangus

--- src/prj5/InfluencerTest.java
+++ src/prj5/InfluencerTest.java
@@ -2,8 +2,8 @@ package prj5;
 
 // -------------------------------------------------------------------------
 /**
- * Write a one-sentence summary of your class here. Follow it with additional
- * details about its purpose, what abstraction it represents, and how to use it.
+ * A test class for the Influencer class, tests all methods and instructions
+ * possible in the class.
  * 
  * @author ddmat
  * @version Apr 18, 2024
@@ -33,7 +33,7 @@ public class InfluencerTest
         iB = new Influencer("B name", "B channel", "France", "Frogs");
 
         month = new Month("January", 2, 4, 6, 8, 10);
-        
+
         iB.addMonthActivity(month);
         iB.addMonthActivity(new Month("February", 2, 4, 6, 8, 10));
         iB.addMonthActivity(new Month("March", 2, 4, 6, 8, 10));
@@ -47,107 +47,123 @@ public class InfluencerTest
     }
 
 // ----------------------------------------------------------
-    
+
+
     /**
      * test the getter methods of the influencer class
      */
-    public void testGetters() 
+    public void testGetters()
     {
         assertEquals("A name", iA.getUsername());
         assertEquals("A channel", iA.getChannelName());
         assertEquals("France", iA.getCountryName());
         assertEquals("Frogs", iA.getPageTopic());
     }
-    
+
+
     /**
-     * test the getTraditionalEngagmentRate for the influencer class with a 
+     * test the getTraditionalEngagmentRate for the influencer class with a
      * month parameter, should return the engagment rate of that month
      */
-    public void testGetTraditionalEngagementRateParameter() 
+    public void testGetTraditionalEngagementRateParameter()
     {
-        assertEquals(((2.0 + 8.0) / 6) * 100.0, 
-            iB.getTraditionalEngagementRate(month), 0.1);
-        assertEquals(-1, 
-            iAZero.getTraditionalEngagementRate(monthZero), 0.1);
+        assertEquals(
+            ((2.0 + 8.0) / 6) * 100.0,
+            iB.getTraditionalEngagementRate(month),
+            0.1);
+        assertEquals(-1, iAZero.getTraditionalEngagementRate(monthZero), 0.1);
     }
+
+
     /**
      * testGetTradititonalEngagementRate with no parameter
      */
-   public void testGetTraditionalEngagementRate() {
-    
-    assertEquals(((3.0 + 3.0) / 1.0) * 100.0, 
-        iA.getTraditionalEngagementRate(), 0.1);
-    assertEquals(-1, 
-        iAZero.getTraditionalEngagementRate(), 0.1);
-    assertEquals(((24 + 6) / 6.0) * 100.0, 
-        iB.getTraditionalEngagementRate(), 0.1);
-    
-    }
-   
-   /**
-    * test the getReachEngagementRate with no parameter to see if it returns 
-    * the correct reach engagment rate or returns -1 if there is no views
-    */
-   public void testGetReachEngagementRate() 
-   {
-       Influencer iAZero2 = new 
-           Influencer("A name", "A channel", "France", "Frogs");
-       
-       iAZero2.addMonthActivity(monthZero);
-       
-       assertEquals(((3.0 + 3.0) / 3.0) * 100.0, 
-           iA.getReachEngagementRate(), 0.1);
-       assertEquals(-1, 
-           iAZero2.getReachEngagementRate(), 0.1);
-       assertEquals(((24 + 6) / 30.0) * 100.0, 
-           iB.getReachEngagementRate(), 0.1);
-   }
-   
-   /**
-    * test the getReachEngagementRate with a month parameter to see if it 
-    * returns the correct reach engagment rate or returns -1 if there is no 
-    * views
-    */
-    public void testGetReachEngagementRateParameter() 
+    public void testGetTraditionalEngagementRate()
     {
-        assertEquals(((2.0 + 8.0) / 10.0) * 100.0, 
-            iB.getReachEngagementRate(month), 0.1);
-        assertEquals(-1, 
-            iAZero.getReachEngagementRate(monthZero), 0.1);
-   }
-    
+
+        assertEquals(
+            ((3.0 + 3.0) / 1.0) * 100.0,
+            iA.getTraditionalEngagementRate(),
+            0.1);
+        assertEquals(-1, iAZero.getTraditionalEngagementRate(), 0.1);
+        assertEquals(
+            ((24 + 6) / 6.0) * 100.0,
+            iB.getTraditionalEngagementRate(),
+            0.1);
+
+    }
+
+
+    /**
+     * test the getReachEngagementRate with no parameter to see if it returns
+     * the correct reach engagment rate or returns -1 if there is no views
+     */
+    public void testGetReachEngagementRate()
+    {
+        Influencer iAZero2 =
+            new Influencer("A name", "A channel", "France", "Frogs");
+
+        iAZero2.addMonthActivity(monthZero);
+
+        assertEquals(
+            ((3.0 + 3.0) / 3.0) * 100.0,
+            iA.getReachEngagementRate(),
+            0.1);
+        assertEquals(-1, iAZero2.getReachEngagementRate(), 0.1);
+        assertEquals(
+            ((24 + 6) / 30.0) * 100.0,
+            iB.getReachEngagementRate(),
+            0.1);
+    }
+
+
+    /**
+     * test the getReachEngagementRate with a month parameter to see if it
+     * returns the correct reach engagment rate or returns -1 if there is no
+     * views
+     */
+    public void testGetReachEngagementRateParameter()
+    {
+        assertEquals(
+            ((2.0 + 8.0) / 10.0) * 100.0,
+            iB.getReachEngagementRate(month),
+            0.1);
+        assertEquals(-1, iAZero.getReachEngagementRate(monthZero), 0.1);
+    }
+
+
     /**
      * test the equals method to see if two influencer objects are equal to each
      * other
      */
-    public void testEquals() 
+    public void testEquals()
     {
         assertTrue(iA.equals(iA));
-        
+
         assertFalse(iA.equals(null));
-        
+
         Object notInfluencer = new Object();
         assertFalse(iA.equals(notInfluencer));
-        
+
         assertFalse(iA.equals(iB));
-        
-        Influencer iADup = new 
-            Influencer("A name", "A channel", "France", "Frogs");
+
+        Influencer iADup =
+            new Influencer("A name", "A channel", "France", "Frogs");
 
         iADup.addMonthActivity(new Month("January", 1, 1, 1, 1, 1));
         iADup.addMonthActivity(new Month("February", 1, 1, 1, 1, 1));
         iADup.addMonthActivity(new Month("March", 1, 1, 1, 1, 1));
-        
+
         assertTrue(iA.equals(iADup));
-        
-        Influencer iADiffChannelName = new 
-            Influencer("A name", "A diffChannel", "France", "Frogs");
-        
+
+        Influencer iADiffChannelName =
+            new Influencer("A name", "A diffChannel", "France", "Frogs");
+
         assertFalse(iA.equals(iADiffChannelName));
-        
-        Influencer iADiffPageTopic = new 
-            Influencer("A name", "A channel", "France", "Bears");
-        
+
+        Influencer iADiffPageTopic =
+            new Influencer("A name", "A channel", "France", "Bears");
+
         assertFalse(iA.equals(iADiffPageTopic));
     }
 }

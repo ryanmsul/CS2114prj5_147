ca727b34615525021d7cd57a9d21da85f43f438a
jimenezjessej05 <jimenezjessej05@vt.edu>
2024-04-23 20:57:12 -0400

    did some comparator magic
    extended the Comparator class with an overloaded Month compare method, but will need to rethink this, going to fix Weds night/ Thursday

---
+++ src/prj5/CompareEngagement.java
@@ -0,0 +1,31 @@
+package prj5;
+
+import java.util.Comparator;
+
+// -------------------------------------------------------------------------
+/**
+ * Extends comparator and adds a new overloaded method to implement, which
+ * allows Month comparing in influencers. While it is generic, this will only be
+ * used by Influencer.
+ * 
+ * @param <T>
+ * @author jesse
+ * @version Apr 23, 2024
+ */
+public interface CompareEngagement<T>
+    extends Comparator<T>
+{
+    // ----------------------------------------------------------
+    /**
+     * Compares two objects, with a third parameter specifying the month.
+     * 
+     * @param o1
+     *            Object 1
+     * @param o2
+     *            Object 2
+     * @param month
+     *            The month object
+     * @return Returns an int based on whether o2 is > o1.
+     */
+    public int compare(T o1, T o2, Month month);
+}
